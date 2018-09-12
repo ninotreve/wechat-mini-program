afed4f67b92096375a89c668b0e97d64a8b8d635
unknown <unknown@example.com>
2018-09-12 18:44:28 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,92 @@
+//index.js
+//获取应用实例
+var voice = "";
+var initData = "朗读文本：\n目前还没有朗读文本"
+var show = "";
+
+Page({
+  record: function () {
+    wx.startRecord({
+      success: function (res) {
+        voice = res.tempFilePath
+      },
+      fail: function (res) {
+        //录音失败
+      }
+    })
+    setTimeout(function () {
+      //结束录音  
+      wx.stopRecord()
+    }, 10000)
+  },
+  stop: function () {
+    wx.stopRecord();
+  },
+  play: function () {
+    wx.playVoice({
+      filePath: voice,
+      complete: function () {
+      }
+    })
+  },
+
+  data: {
+    text: initData
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
